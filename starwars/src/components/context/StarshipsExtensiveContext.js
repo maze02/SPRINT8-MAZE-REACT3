7147670a5e0c1e401bfa67fa07b9e710a37ecc70
maze02 <71835134+maze02@users.noreply.github.com>
2021-07-23 19:14:47 +0200

    get pilot url works

--- starwars/src/components/context/StarshipsExtensiveContext.js
+++ starwars/src/components/context/StarshipsExtensiveContext.js
@@ -5,8 +5,9 @@ import { StarshipsContext } from "../context/StarshipsContext";
 
 const StarshipExtensiveProvider = (props) => {
   const [singleShip, setSingleShip] = useState();
-  const [pilots, setPilots] = useState([]);
-  const [films, setFilms] = useState([]);
+  const [pilotUrls, setPilotUrls] = useState([]);
+  const [pilotInfo, setPilotInfo] = useState([]);
+  const [filmUrls, setFilmUrls] = useState([]);
   const ctx = useContext(StarshipsContext);
   const history = useHistory();
 
@@ -19,13 +20,35 @@ const StarshipExtensiveProvider = (props) => {
     const getSingleShip = async () => {
       const url = `https://swapi.dev/api/starships/${e.target.id}/`;
       const singleShipObj = await axios.get(url);
-      setSingleShip(singleShipObj.data);
+      await setSingleShip(singleShipObj.data);
       console.log("singleShipObj.name" + singleShipObj.name);
+      await setPilotUrls(singleShipObj.data.pilots);
+      await setFilmUrls(singleShipObj.data.films);
+      console.log("length of pilots arr =" + pilotUrls.length);
+      console.log("length of films arr =" + filmUrls.length);
     };
     getSingleShip();
-    //history.replace("/home");
+
     history.push(`/starship-detail/:${e.target.id}`);
   };
+
+  //Call each of Pilot Apis and add info into an arr for child to map through
+  useEffect(() => {
+    if (pilotUrls.length > 0) {
+      for (let i = 0; i < pilotUrls.length; i++) {
+        //let pilotObj = "";
+        const getPilot = async () => {
+          const pilotObj = await axios.get(pilotUrls[i]);
+          console.log("printing" + i + " " + pilotObj.data.name);
+          setPilotInfo([...pilotInfo, pilotObj.data]);
+        };
+        getPilot();
+
+        console.log("Pilot" + i);
+      }
+      //console.log("pilotInfo.length" + pilotInfo.length);
+    }
+  }, [pilotUrls]);
 
   return (
     <StarshipExtensiveCtx.Provider
@@ -41,3 +64,16 @@ const StarshipExtensiveProvider = (props) => {
 
 export default StarshipExtensiveProvider;
 export const StarshipExtensiveCtx = createContext();
+
+/*
+    if (pilotUrls.length > 0) {
+      const getPilotObj = async (urlPilot) => {
+        const pilotObj = await axios.get(urlPilot.data);
+        await setPilotInfo([...pilotInfo, pilotObj]);
+      };
+      for (let i = 0; i < pilotUrls.length; i++) {
+        getPilotObj(pilotUrls[i]);
+      }
+      console.log("pilotInfo.length" + pilotInfo.length);
+    }
+*/
