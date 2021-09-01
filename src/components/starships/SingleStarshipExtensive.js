eb99f0e299d76de71c364df373a94711c6b6d368
maze02 <71835134+maze02@users.noreply.github.com>
2021-09-01 17:47:56 +0200

    SiggleStarshipExtensive component refactored as a styled component

--- src/components/starships/SingleStarshipExtensive.js
+++ src/components/starships/SingleStarshipExtensive.js
@@ -1,5 +1,5 @@
+import styled from "styled-components";
 import { Fragment, useContext, useState, useEffect } from "react";
-import classes from "./SingleStarshipExtensive.module.css";
 import { StarshipExtensiveCtx } from "../context/StarshipsExtensiveCtx";
 
 const SingleStarshipExtensive = ({ loadShip }) => {
@@ -24,11 +24,11 @@ const SingleStarshipExtensive = ({ loadShip }) => {
       {!loadShip && singleShip && (
         <Fragment>
           <h1>{singleShip.name}</h1>
-          <div className={classes.wrapperMain}>
-            <div className={classes.cardMain}>
+          <Wrapper>
+            <div className="cardMain">
               {!error && (
                 <img
-                  className={classes.imgMain}
+                  className="imgMain"
                   type="url"
                   alt={"starship" + singleShip.name}
                   src={shipUrl}
@@ -38,12 +38,12 @@ const SingleStarshipExtensive = ({ loadShip }) => {
                 ></img>
               )}
               {error && (
-                <p className={classes.imgErrMain}>
+                <p className="imgErrMain">
                   Image of {singleShip.name} starship is not available at this
                   moment
                 </p>
               )}
-              <ul className={classes.info}>
+              <ul className="info">
                 <li>Model:{singleShip.model}</li>
                 <li>Cost in credits:{singleShip.cost_in_credits}</li>
                 <li>Manufacturer:{singleShip.manufacturer}</li>
@@ -56,10 +56,55 @@ const SingleStarshipExtensive = ({ loadShip }) => {
                 <li>Passengers:{singleShip.passengers}</li>
               </ul>
             </div>
-          </div>
+          </Wrapper>
         </Fragment>
       )}
     </div>
   );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: horizontal;
+  justify-content: center;
+
+  .cardMain {
+    border-radius: 2px;
+    background-color: #222222;
+    box-shadow: 0 3px 8px rgba(0, 0, 0, 0.8);
+    width: 140rem !important;
+    height: 40rem;
+    display: grid;
+    grid-template-columns: 5fr 3fr;
+    margin-bottom: 2rem;
+    border-radius: 0.7rem;
+  }
+
+  .imgErrMain {
+    display: flex;
+    text-align: center;
+    margin-left: 25%;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    align-self: center;
+  }
+  .imgMain {
+    border-radius: 0.7rem 0rem 0rem 0.7rem;
+    border-right: 0.3rem solid #3273c5;
+    width: 50rem;
+    height: 40rem;
+    margin-right: 2rem;
+    object-fit: cover;
+  }
+
+  .info {
+    padding: auto 5rem auto 5rem;
+    display: flex;
+    flex-direction: column;
+    color: #dadada;
+    line-height: 3rem;
+    justify-content: center;
+  }
+`;
 export default SingleStarshipExtensive;
