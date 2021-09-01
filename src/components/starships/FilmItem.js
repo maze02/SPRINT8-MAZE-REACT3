ca3a4bbd2bd7dc33686d1f5948387ae815e4aa9a
maze02 <71835134+maze02@users.noreply.github.com>
2021-09-01 13:46:06 +0200

    Film Item refactored into a styled component

--- src/components/starships/FilmItem.js
+++ src/components/starships/FilmItem.js
@@ -1,20 +1,47 @@
-import classes from "./FilmItem.module.css";
+import styled from "styled-components";
+
 const FilmItem = ({ title, release_date, filmImg }) => {
   return (
-    <div className={classes.cardFilm}>
-      <img className={classes.imgFilm} type="url" src={filmImg} alt={title} />
-      <ul className={classes.info}>
+    <Wrapper>
+      <img type="url" src={filmImg} alt={title} />
+      <ul>
         <li>"{title.toUpperCase()}"</li>
         <li>{release_date}</li>
       </ul>
-    </div>
+    </Wrapper>
   );
 };
 
 export default FilmItem;
+
+const Wrapper = styled.div`
+  border-radius: 2px;
+  background-color: #282727;
+  box-shadow: 0 3px 8px rgba(0, 0, 0, 0.8);
+  text-align: left;
+  width: 20rem;
+  height: auto;
+  display: grid;
+  grid-template-rows: 4fr 1fr;
+  margin: 0rem 1.3rem 2rem 1.3rem;
+  border-radius: 0.7rem;
+
+  img {
+    width: 20rem;
+    height: auto;
+    border-radius: 0.7rem 0.7rem 0rem 0rem;
+    border-bottom: 0.3rem solid #3273c5;
+    object-fit: cover;
+    margin-bottom: 2rem;
+  }
+
+  ul {
+    padding: 0rem 2rem 0rem 2rem;
+    color: #dadada;
+    line-height: 2rem;
+  }
+`;
 //<li>{planet}</li>
 /* planet is a URL
 would have to do an other API call
 */
-
-
