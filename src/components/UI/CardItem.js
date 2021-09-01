2dac49fb122fb28ff102a05a4d113070b206d9b4
maze02 <71835134+maze02@users.noreply.github.com>
2021-09-01 13:27:12 +0200

    refactored card item into a styled component

--- src/components/UI/CardItem.js
+++ src/components/UI/CardItem.js
@@ -1,15 +1,26 @@
-import classes from "./Card.module.css";
+import styled from "styled-components";
 
 const CardItem = ({ children, id, pressed }) => {
   const clickItem = () => {
     pressed(id);
   };
-  return (
-    <div className={classes.card} onClick={clickItem}>
-      {children}
-    </div>
-  );
+  return <Wrapper onClick={clickItem}>{children}</Wrapper>;
 };
 
-export default CardItem;
+const Wrapper = styled.div`
+  padding: 0.1rem 0rem 0.1rem 2rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 2px;
+  background-color: #222222;
+  text-align: left;
+  margin-bottom: 2rem;
+  width: 60rem;
+  border-radius: 0.7rem;
 
+  &:hover {
+    background-color: #333333;
+    cursor: pointer;
+  }
+`;
+
+export default CardItem;
